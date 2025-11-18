747720292da8f80319cbb19c548e262c202563c5
giannivitale <giavitale78@gmail.com>
2025-11-18 15:56:19 +0100

    refactor Header component and create Navbar component

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,61 @@
+import { NavLink } from "react-router-dom";
+
+const menu = [
+    {
+        id: 1,
+        text: 'Home',
+        link: '/'
+    },
+    {
+        id: 2,
+        text: 'Blog',
+        link: '/posts'
+    },
+    {
+        id: 3,
+        text: 'About',
+        link: '/about'
+    },
+    {
+        id: 4,
+        text: 'Contacts',
+        link: '/contacts'
+    },
+    {
+        id: 5,
+        text: 'Products',
+        link: '/products'
+    }
+];
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-md navbar-light bg-light">
+            <div className="container-fluid">
+                <button
+                    className="navbar-toggler d-lg-none"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapsibleNavId"
+                    aria-controls="collapsibleNavId"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="collapsibleNavId">
+                    <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+                        {
+                            menu.map(item => (
+                                <li className="nav-item" key={item.id}>
+                                    <NavLink className="nav-link" to={item.link} aria-current="page">
+                                        {item.text}
+                                    </NavLink>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
